e3c980cd0e33841b0b9a7335e95de2a669ad9b91
asokarakha55-cloud <asokarakha55@gmail.com>
2026-05-04 13:46:28 +0700

    Membuat class buku

--- Chainfunctional.cpp
+++ Chainfunctional.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class buku
+{
+    string judul;
+
+public:
+    buku setjudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain functional
+    }
+};
